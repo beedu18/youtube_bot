37965c0999d18dd91f2f226fd0bf4bdcbdd6ba4f
beedu18 <b1n4y4k@gmail.com>
2022-03-26 21:18:33 +0530

    playback speed and resolution change working.

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ link = 'https://www.youtube.com/watch?v=i44g8-FrAU8'
 settings_btn = '//*[@id="movie_player"]/div[26]/div[2]/div[2]/button[4]'
 
 # in seconds
-long_delay = 3
-short_delay = 2
-vs_delay = 0.1
-watch_time = 30
+long_delay = 3 # to allow page to load
+vs_delay = 0.2 # for keyboard events
+watch_time = 10 
 
+# for generating random seek taps
 min_ = 1
 max_ = 100
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
 
+def time_format(string, time):
+    print(f'{string}: {int((time)/60)} minutes and {(time)%60} seconds')
+
 def initialize():
     options = webdriver.ChromeOptions()
     options.add_argument("--incognito")
@@ -19,18 +22,58 @@ def initialize():
 def click_settings(driver):
     settings = driver.find_element(By.XPATH, config.settings_btn)
     settings.click()
-    time.sleep(config.short_delay)
-    webdriver.ActionChains(driver).key_down(Keys.ESCAPE).perform()
+    time.sleep(config.vs_delay)
+    # webdriver.ActionChains(driver).key_down(Keys.ESCAPE).perform()
 
 def change_resolution(driver):
-    pass
+    # DN2 ENT UP1 ENT ESC
+    click_settings(driver)
+
+    webdriver.ActionChains(driver).key_down(Keys.ARROW_DOWN).perform()
+    time.sleep(config.vs_delay)
+    
+    webdriver.ActionChains(driver).key_down(Keys.ARROW_DOWN).perform()
+    time.sleep(config.vs_delay)
+    
+    webdriver.ActionChains(driver).key_down(Keys.ENTER).perform()
+    time.sleep(config.vs_delay)
+    
+    webdriver.ActionChains(driver).key_down(Keys.ARROW_UP).perform()
+    time.sleep(config.vs_delay)
+    
+    webdriver.ActionChains(driver).key_down(Keys.ENTER).perform()
+    time.sleep(config.vs_delay)
+    
+    webdriver.ActionChains(driver).key_down(Keys.ESCAPE).perform()
+    time.sleep(config.vs_delay)
 
 def change_speed(driver):
-    pass
+    click_settings(driver)
+
+    webdriver.ActionChains(driver).key_down(Keys.ENTER).perform()
+    time.sleep(config.vs_delay)
+
+    webdriver.ActionChains(driver).key_down(Keys.ARROW_DOWN).perform()
+    time.sleep(config.vs_delay)
+
+    webdriver.ActionChains(driver).key_down(Keys.ARROW_DOWN).perform()
+    time.sleep(config.vs_delay)
+
+    webdriver.ActionChains(driver).key_down(Keys.ARROW_DOWN).perform()
+    time.sleep(config.vs_delay)
+
+    webdriver.ActionChains(driver).key_down(Keys.ARROW_DOWN).perform()
+    time.sleep(config.vs_delay)
+
+    webdriver.ActionChains(driver).key_down(Keys.ENTER).perform()
+    time.sleep(config.vs_delay)
+
+    webdriver.ActionChains(driver).key_down(Keys.ESCAPE).perform()
+    time.sleep(config.vs_delay)
 
 def seek_video(driver):
     num_times = random.randint(config.min_, config.max_)
-    print(f'Video seeking for {int((num_times * 5)/60)} minutes and {(num_times * 5)%60} seconds')
+    time_format('Seeking', num_times * 5)
     while (num_times > 0):
         # print(num_times)
         webdriver.ActionChains(driver).key_down(Keys.ARROW_RIGHT).perform()
@@ -41,13 +84,17 @@ def main():
     while True:
         driver = initialize()
 
-        click_settings(driver)
+        # click_settings(driver)
 
         seek_video(driver)
 
-        # watch time
-        print(f'Video will play for {int((config.watch_time)/60)} minutes and {(config.watch_time)%60} seconds')
-        time.sleep(watch_time)
+        change_resolution(driver)
+
+        change_speed(driver)
+
+        # watch time can be made dynamic using randint
+        time_format('Playback Time', config.watch_time)
+        time.sleep(config.watch_time)
 
         driver.quit()
 
